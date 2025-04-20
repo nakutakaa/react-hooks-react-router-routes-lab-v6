b9126324627cb38ff3fdbc6951fecd9909ad7cdd
Mkalabai John <nakutakaa@gmail.com>
2025-04-20 16:21:43 +0300

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
+import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import routes from "./routes";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render();
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import ErrorPage from "./pages/ErrorPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />,
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+  },
+]);
 
-const routes = [
-   
-  ];
-
-export default routes;
+export default router;
